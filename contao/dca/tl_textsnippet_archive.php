1d2032e4e38620ce3bd30c3c81f33a18abb117ae
Florian Otto <florian.otto@mindbird.de>
2024-11-21 12:09:12 +0100

    Fix PHP8 warnings

--- contao/dca/tl_textsnippet_archive.php
+++ contao/dca/tl_textsnippet_archive.php
@@ -60,7 +60,7 @@ $GLOBALS ['TL_DCA'] ['tl_textsnippet_archive'] = array (
                 'label' => &$GLOBALS ['TL_LANG'] ['tl_textsnippet_archive'] ['delete'],
                 'href' => 'act=delete',
                 'icon' => 'delete.gif',
-                'attributes' => 'onclick="if(!confirm(\'' . $GLOBALS ['TL_LANG'] ['MSC'] ['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+                'attributes' => 'onclick="if(!confirm(\'' . $GLOBALS ['TL_LANG'] ['tl_textsnippet_archive'] ['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
             )
         )
     ),
--- contao/languages/de/tl_textsnippet_archive.php
+++ contao/languages/de/tl_textsnippet_archive.php
@@ -4,6 +4,7 @@ $GLOBALS ['TL_LANG'] ['tl_textsnippet_archive'] ['editheader'] = array('Archivei
 $GLOBALS ['TL_LANG'] ['tl_textsnippet_archive'] ['edit'] = array('Archiv öffnen', '');
 $GLOBALS ['TL_LANG'] ['tl_textsnippet_archive'] ['copy'] = array('Archiv kopieren', '');
 $GLOBALS ['TL_LANG'] ['tl_textsnippet_archive'] ['delete'] = array('Archiv löschen', '');
+$GLOBALS ['TL_LANG'] ['tl_textsnippet_archive'] ['delete'] = array('Soll der Eintrag wirklich gelöscht werden?', '');
 $GLOBALS ['TL_LANG'] ['tl_textsnippet_archive'] ['new'] = array('Neues Archiv', '');
 
 $GLOBALS ['TL_LANG'] ['tl_textsnippet_archive'] ['title_legend'] = 'Allgemein';
